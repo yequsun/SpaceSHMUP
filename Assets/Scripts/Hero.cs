9e329ff5311e71ccb38c10ba758e723fe7c77591
Yequ Sun <yequsun@gmail.com>
2015-10-22 18:50:13 -0400

    1

---
+++ Assets/Scripts/Hero.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Hero : MonoBehaviour {
+
+	static public Hero S;
+
+	public float speed = 30;
+	public float rollMult = -45;
+	public float pitchMult = 30;
+
+	public float shieldLevel = 1;
+	public bool ___________;
+	public Bounds bounds;
+
+	void Awake(){
+		S = this;
+		bounds = Util.CombineBoundsOfChildren (this.gameObject);
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float xAxis = Input.GetAxis ("Horizontal");
+		float yAxis = Input.GetAxis ("Vertical");
+
+		Vector3 pos = transform.position;
+		pos.x += xAxis * speed * Time.deltaTime;
+		pos.y += yAxis * speed * Time.deltaTime;
+		transform.position = pos;
+
+		bounds.center = transform.position;
+
+		Vector3 off = Util.ScreenBoundsCheck (bounds, BoundsTest.onScreen);
+		if (off != Vector3.zero) {
+			pos -= off;
+			transform.position = pos;
+		}
+
+		transform.rotation = Quaternion.Euler (yAxis * pitchMult, xAxis * rollMult, 0);
+	
+	}
+}
---
+++ Assets/Scripts/Util.cs
@@ -0,0 +1,172 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public enum BoundsTest{
+	center,
+	onScreen,
+	offScreen
+}
+
+public class Util : MonoBehaviour {
+	public static Bounds BoundsUnion(Bounds b0,Bounds b1){
+		if (b1.size == Vector3.zero && b1.size != Vector3.zero) {
+			return b1;
+		} else if (b0.size != Vector3.zero && b1.size == Vector3.zero) {
+			return b0;
+		} else if (b0.size == Vector3.zero && b1.size == Vector3.zero) {
+			return b0;
+		}
+		b0.Encapsulate (b1.min);
+		b0.Encapsulate (b1.max);
+		return b0;
+	}
+
+	public static Bounds CombineBoundsOfChildren(GameObject go){
+		Bounds b = new Bounds (Vector3.zero, Vector3.zero);
+
+		if (go.renderer != null) {
+			b = BoundsUnion(b,go.renderer.bounds);
+		}
+
+		if (go.collider != null) {
+			b = BoundsUnion(b,go.collider.bounds);
+		}
+
+		foreach (Transform t in go.transform) {
+			b = BoundsUnion(b,CombineBoundsOfChildren(t.gameObject));
+		}
+
+		return b;
+	}
+
+	public static Bounds camBounds{
+		get{
+			if(_camBounds.size == Vector3.zero){
+				SetCameraBounds();
+			}
+			return _camBounds;
+		}
+	}
+
+	static private Bounds _camBounds;
+
+	public static void SetCameraBounds(Camera cam = null){
+		if (cam == null) {
+			cam = Camera.main;
+		}
+		Vector3 topLeft = new Vector3 (0, 0, 0);
+		Vector3 bottomRight = new Vector3 (Screen.width, Screen.height, 0);
+
+		Vector3 boundTLN = cam.ScreenToWorldPoint (topLeft);
+		Vector3 boundBRF = cam.ScreenToWorldPoint (bottomRight);
+
+		boundTLN.z += cam.nearClipPlane;
+		boundBRF.z += cam.farClipPlane;
+
+		Vector3 center = (boundTLN + boundBRF) / 2f;
+		_camBounds = new Bounds (center, Vector3.zero);
+		_camBounds.Encapsulate (boundTLN);
+		_camBounds.Encapsulate (boundBRF);
+
+	}
+
+	public static Vector3 ScreenBoundsCheck(Bounds bnd,BoundsTest test = BoundsTest.center){
+		return BoundsInBoundsCheck (camBounds, bnd, test);
+	}
+
+	public static Vector3 BoundsInBoundsCheck(Bounds bigB, Bounds lilB, BoundsTest test = BoundsTest.onScreen){
+		Vector3 pos = lilB.center;
+		Vector3 off = Vector3.zero;
+
+		switch (test) {
+		case BoundsTest.center:
+			if (bigB.Contains (pos)) {
+				return Vector3.zero;
+			}
+
+			if (pos.x > bigB.max.x) {
+				off.x = pos.x - bigB.max.x;
+			} else if (pos.x < bigB.min.x) {
+				off.x = pos.x - bigB.min.x;
+			}
+
+			if (pos.y > bigB.max.y) {
+				off.y = pos.y - bigB.max.y;
+			} else if (pos.y < bigB.min.y) {
+				off.y = pos.y - bigB.min.y;
+			}
+
+			if (pos.z > bigB.max.z) {
+				off.z = pos.z - bigB.max.z;
+			} else if (pos.z < bigB.min.z) {
+				off.z = pos.z - bigB.min.z;
+			}
+
+			return (off);
+		case BoundsTest.onScreen:
+			if (bigB.Contains (lilB.min) && bigB.Contains (lilB.max)) {
+				return Vector3.zero;
+			}
+
+			if (lilB.max.x > bigB.max.x) {
+				off.x = lilB.max.x - bigB.max.x;
+			} else if (lilB.min.x < bigB.min.x) {
+				off.x = lilB.min.x - bigB.min.x;
+			}
+			
+			if (lilB.max.y > bigB.max.y) {
+				off.y = lilB.max.y - bigB.max.y;
+			} else if (lilB.min.y < bigB.min.y) {
+				off.y = lilB.min.y - bigB.min.y;
+			}
+			
+			if (lilB.max.z > bigB.max.z) {
+				off.z = lilB.max.z - bigB.max.z;
+			} else if (lilB.min.z < bigB.min.z) {
+				off.z = lilB.min.z - bigB.min.z;
+			}
+			return off;
+
+		case BoundsTest.offScreen:
+			bool cMin = bigB.Contains (lilB.min);
+			bool cMax = bigB.Contains (lilB.max);
+
+			if (cMax || cMin) {
+				return Vector3.zero;
+			}
+
+			if (lilB.min.x > bigB.max.x) {
+				off.x = lilB.min.x - bigB.max.x;
+			} else if (lilB.max.x < bigB.min.x) {
+				off.x = lilB.max.x - bigB.min.x;
+			}
+			
+			if (lilB.min.y > bigB.max.y) {
+				off.y = lilB.min.y - bigB.max.y;
+			} else if (lilB.max.y < bigB.min.y) {
+				off.y = lilB.max.y - bigB.min.y;
+			}
+			
+			if (lilB.min.z > bigB.max.z) {
+				off.z = lilB.min.z - bigB.max.z;
+			} else if (lilB.max.z < bigB.min.z) {
+				off.z = lilB.max.z - bigB.min.z;
+			}
+			return off;
+
+		}
+		return Vector3.zero;
+	}
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
